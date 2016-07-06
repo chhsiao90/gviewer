1c28a5d1f5fbffd533874e698acedef7e1ccd660
chhsiao90 <chhsiao90@gmail.com>
2016-07-06 17:59:55 +0800

    Solve #3 refine setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,33 @@
-from distutils.core import setup
+from setuptools import setup
 
 
-setup(name="gviewer",
-      version="0.0.1",
-      description="General Viewer",
-      author="chhsiao90",
-      author_email="chhsiao90@gmail.com",
-      packages=["gviewer"])
+setup(
+    name="gviewer",
+    version="0.0.2",
+    description="General Viewer",
+    author="chhsiao90",
+    author_email="chhsiao90@gmail.com",
+    url="https://github.com/chhsiao90/gviewer",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Environment :: Console",
+        "Environment :: Console :: Curses",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Security",
+        "Topic :: Internet",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: Proxy Servers",
+        "Topic :: Software Development :: Testing"
+    ],
+    packages=["gviewer"],
+    install_requires=[
+        "urwid==1.3.1"
+    ]
+)
