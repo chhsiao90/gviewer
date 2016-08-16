ffeaa7477cc1d655a13ed762c6010e64c8cc72d2
chhsiao90 <chhsiao90@gmail.com>
2016-08-16 08:00:38 +0800

    Add docstring

--- gviewer/gviewer.py
+++ gviewer/gviewer.py
@@ -3,25 +3,14 @@ from parent import ParentFrame
 from config import Config
 
 
-"""A General Purpose tui Viewer library that based on urwid """
-
-
 class GViewer(object):
-    """
-    General Viewer Main Class
-    :param data_store:
-    :type data_store: BaseDataStore Implementation
+    """ General viewer main class
 
-    :param displayer:
-    :type displayer: BaseDisplayer implmementation
-
-    :param palette:
-    :type palette: iterable for patette entries
-
-    :param config: GViewer general config
-    :type config: Config
-
-    :param **kwargs: other urwid.MainLoop supported parameters
+    Attributes:
+        data_store: a BaseDataStore implementation instance
+        displayer: a BaseDisplayer implementation instance
+        config: a Config instance
+        others: any other args defined in urwid.MainLoop
     """
     def __init__(self, data_store, displayer, config=None, **kwargs):
         self.config = config or Config()
@@ -34,23 +23,30 @@ class GViewer(object):
         self.data_store = data_store
 
     def _default_urwid_options(self, kwargs):
+        """ generate default urwid options
+
+        handle_mouse with False
+        unhandled_input with "q", "Q" to exit tui
+        palette with predefined template in gviewer
+
+        """
         if "handle_mouse" not in kwargs:
             kwargs["handle_mouse"] = False
 
         if "unhandled_input" not in kwargs:
-            kwargs["unhandled_input"] = self.default_unhandled_input
+            kwargs["unhandled_input"] = self._default_unhandled_input
 
         if "palette" in kwargs:
             kwargs["palette"] = self.config.template + kwargs["palette"]
         else:
             kwargs["palette"] = self.config.template
 
-    def default_unhandled_input(self, key):
+    def _default_unhandled_input(self, key):
         if key in ("q", "Q"):
             raise urwid.ExitMainLoop()
 
     def start(self):
-        """ Use this method to start tui
+        """ Start the gviewer tui
         """
         self.data_store.set_up()
         self.loop.run()
--- gviewer/summary.py
+++ gviewer/summary.py
@@ -3,19 +3,17 @@ import urwid
 
 from basic import BasicWidget, FocusableText, SearchWidget
 
-""" Summary Widget Related Component
-
-Contents:
-
-* `SummaryItemWidget`: One line of the SummaryList
-* `SummaryListWalker`: The urwid Walker to iterate over the data_store
-* `FilterSummaryListWalker`: The urwid Walker to iterate over the data_store with filter
-* `SummaryListWidget`: urwid ListBox to display Summary
-
-"""
-
 
 class SummaryItemWidget(BasicWidget):
+    """ Summary item widget
+
+    Summary item widget that display summary and defined action to open other view
+
+    Attributes:
+        parent: ParentFrame
+        message: Original message genrate by BaseDataStore
+        summary: Format message by displayer
+    """
     def __init__(self, parent, message, summary):
         super(SummaryItemWidget, self).__init__(
             parent=parent,
@@ -28,6 +26,11 @@ class SummaryItemWidget(BasicWidget):
         self.message = message
 
     def get_title(self):
+        """ Get summary in plain text
+
+        Returns:
+            summary in plain text
+        """
         return self._w.get_plain_text()
 
     def keypress(self, size, key):
@@ -38,12 +41,26 @@ class SummaryItemWidget(BasicWidget):
 
 
 class SummaryListWalker(urwid.SimpleFocusListWalker):
+    """ Summary item widgets wrapper
+
+    Contains the SummaryItemWidget,
+    and used to receive message from data store
+
+    Attributes:
+        parent: ParentFrame
+        content: Array of SummaryItemWidget
+    """
     def __init__(self, parent, content=None):
         super(SummaryListWalker, self).__init__(content or [])
         self.parent = parent
         self.parent.msg_listener.register(self)
 
     def recv(self, message):
+        """ Action when received message from data store
+
+        Will transform message into summary,
+        and generate a SummaryItemWidget into its content
+        """
         try:
             summary = self.parent.displayer.summary(message)
             self.append(SummaryItemWidget(self.parent, message, summary))
@@ -52,49 +69,70 @@ class SummaryListWalker(urwid.SimpleFocusListWalker):
 
 
 class FilterSummaryListWalker(SummaryListWalker):
-    def __init__(self, origin_walker, search):
+    """ Summary item widgets wrapper that filter by keyword
+
+    Optional display SummaryItemWidget depend on summary is match by keyword or not
+
+    Attributes:
+        origin_walker: Original SummaryListWalker
+        keyword: Filter keyword
+    """
+    def __init__(self, origin_walker, keyword):
         parent = origin_walker.parent
-        content = [m for m in origin_walker if parent.displayer.match(search, m.message, m.get_title())]
+        content = [m for m in origin_walker if parent.displayer.match(keyword, m.message, m.get_title())]
         super(FilterSummaryListWalker, self).__init__(parent, content=content)
-        self.search = search
+        self.keyword = keyword
 
     def recv(self, message):
+        """ Action when received message from data store
+
+        Will transform message into summary,
+        and check message or summary is match by keyword or not,
+        generate a SummaryItemWidget and display it if matchj
+        """
         try:
             summary = self.parent.displayer.summary(message)
-            if self.parent.displayer.match(self.search, message, summary):
+            if self.parent.displayer.match(self.keyword, message, summary):
                 self.append(SummaryItemWidget(self.parent, message, summary))
         except:
             self.parent.open_error(sys.exc_info())
 
     def close(self):
+        """ Unregister listener if quit search mode """
         self.parent.msg_listener.unregister(self)
 
 
 class SummaryListWidget(BasicWidget):
+    """ ListBox widget to contains the content of SummaryItemWidget
+
+    Attributes:
+        walker: SummaryListWalker instance
+        parent: ParentFrame instance
+    """
     def __init__(self, walker, parent):
         super(SummaryListWidget, self).__init__(parent)
         self.base_walker = walker
         self.current_walker = walker
         self.list_box = urwid.ListBox(walker)
-        self.search_widget = SearchWidget(self.filter, self.clear_search)
+        self.search_widget = SearchWidget(self._filter, self._clear_search)
 
         widget_list = [self.list_box]
         widget = urwid.Pile(widget_list)
         self.display(widget)
 
-    def filter(self, keyword):
+    def _filter(self, keyword):
         if keyword:
             new_walker = FilterSummaryListWalker(self.base_walker, keyword)
         else:
             new_walker = self.base_walker
 
         if new_walker is not self.current_walker:
-            self.update(new_walker)
+            self._update(new_walker)
 
-        self.close_search()
+        self._close_search()
         self._w.focus_position = 0
 
-    def update(self, walker):
+    def _update(self, walker):
         if self.current_walker is not self.base_walker:
             self.current_walker.close()
 
@@ -102,7 +140,7 @@ class SummaryListWidget(BasicWidget):
         self._w.contents.pop(0)
         self._w.contents.insert(0, (urwid.ListBox(walker), self._w.options()))
 
-    def open_search(self):
+    def _open_search(self):
         self.search_widget.clear()
         if len(self._w.contents) == 1:
             self._w.contents.append((
@@ -111,12 +149,12 @@ class SummaryListWidget(BasicWidget):
             )
         self._w.focus_position = 1
 
-    def close_search(self):
+    def _close_search(self):
         if len(self._w.contents) == 2:
             del self._w.contents[1]
 
-    def clear_search(self):
-        self.filter(None)
+    def _clear_search(self):
+        self._filter(None)
 
     def is_editing(self):
         return self._w.focus is self.search_widget
@@ -125,10 +163,10 @@ class SummaryListWidget(BasicWidget):
         if self.is_editing():
             return super(SummaryListWidget, self).keypress(size, key)
         if key == "/":
-            self.open_search()
+            self._open_search()
             return None
         if key == "q" and isinstance(self.current_walker, FilterSummaryListWalker):
-            self.clear_search()
+            self._clear_search()
             return None
 
         return super(SummaryListWidget, self).keypress(size, key)
