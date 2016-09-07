c260200621681066175dd3e50f1616d241b3b0b4
chhsiao90 <chhsiao90@gmail.com>
2016-09-07 14:50:10 +0800

    Update setup.py to v3.0.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="gviewer",
-    version="3.0.1",
+    version="3.0.2",
     description="General Viewer",
     long_description=long_description,
     author="chhsiao90",
