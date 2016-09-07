30f3ca179e59be8d97892799284f5cb65f25a775
chhsiao90 <chhsiao90@gmail.com>
2016-09-07 14:27:18 +0800

    Fixed pygments style not load problem

--- gviewer/styles/default.py
+++ gviewer/styles/default.py
@@ -1,5 +1,28 @@
-from pygments import pygments_style
-
+try:
+    from pygments import token
+except:  # pragma: no cover
+    pygments_style = []
+else:
+    pygments_style = [
+        (token.Token.Text, "white", "black"),
+        (token.Token.Name, "white", "black"),
+        (token.Token.Name.Builtin, "light magenta", "black"),
+        (token.Token.Name.Function, "light magenta", "black"),
+        (token.Token.Name.Class, "light magenta", "black"),
+        (token.Token.Comment, "dark gray", "black"),
+        (token.Token.Literal, "brown", "black"),
+        (token.Token.Literal.String, "brown", "black"),
+        (token.Token.Literal.String.Number, "light cyan", "black"),
+        (token.Token.Literal.String.Double, "light cyan", "black"),
+        (token.Token.Literal.String.Escape, "light cyan", "black"),
+        (token.Token.Literal.Number.Integer, "brown", "black"),
+        (token.Token.Operator, "white", "black"),
+        (token.Token.Error, "white", "black"),
+        (token.Token.Other, "white", "black"),
+        (token.Token.Keyword, "dark cyan", "black"),
+        (token.Token.Keyword.Namespace, "light green", "black"),
+        (token.Token.Generic, "white", "black")
+    ]
 
 palette = [
     ("header", "white", "dark green", "bold"),
