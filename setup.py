c3f36c98a5f42ecc150eeb0a1ce00ba76b86751f
chhsiao90 <chhsiao90@gmail.com>
2016-09-08 13:18:11 +0800

    Release v3.0.3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="gviewer",
-    version="3.0.2",
+    version="3.0.3",
     description="General Viewer",
     long_description=long_description,
     author="chhsiao90",
